24d105e37b2b32eb7b57a6075bb8bd34f114bbed
abstanzeeeeee <abstanzee@gmail.com>
2016-09-09 17:13:37 +0430

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ if (isset($update->edited_message)){
   //$up = file_get_contents(__DIR__.'/users/'.$eid.'.json');
   //str_replace("edited_message","message",$up);
 }elseif(preg_match('/^\/([Ss]tart)/',$text1)){
-  $text = "به ربات ادیت نکن\nخوش آمدید\nبرای اد کردن من به گروه بر روی لینک زیر بزنید\nhttps://telegram.me/DontEdit_BOT?startgroup=new";
+  $text = "به ربات ادیت نکن\nخوش آمدید\nبرای اد کردن من به گروه بر روی لینک زیر بزنید\n    https://telegram.me/editnakonazizbot?startgroup=new;"
   bot('sendmessage',[
     'chat_id'=>$chat_id,
     'text'=>$text,
@@ -53,10 +53,10 @@ if (isset($update->edited_message)){
     'reply_markup'=>json_encode([
       'inline_keyboard'=>[
         [
-          ['text'=>'alireza_PT','url'=>'https://telegram.me/alireza_PT']
+          ['text'=>'MaxTeam 🌹','url'=>'https://telegram.me/MaXTeamCh']
         ],
         [
-          ['text'=>'CreateBOT','url'=>'https://telegram.me/create_antispam_bot']
+          ['text'=>'ADD ME TO GROUP�','url'=>'https://telegram.me/editnakonazizbot?startgroup=new']
         ]
       ]
     ])
